5791150235d0d253fa912b17ac3dd7f9c344efab
Angel Romero <aromero@isn.com>
2018-10-10 21:17:30 -0500

    Chapter 4 Routing

---
+++ Chapter4Routing/Startup.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Routing;
+
+namespace Chapter4Routing
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRouting();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouter(builder =>
+            {
+                builder.MapRoute(string.Empty, context =>
+                { 
+                     return context.Response.WriteAsync($"Welcome to the default route!");
+                });
+
+                builder.MapGet("foo/{name}/{surname?}", (request, response, routeData) =>
+                {
+                    return response.WriteAsync($"Welcome to Foo, {routeData.Values["name"]} {routeData.Values["surname"]}");
+                });
+
+                builder.MapPost("bar/{number:int}", (request, response, routeData) =>
+                 {
+                     return response.WriteAsync($"Welcome to Bar, number is {routeData.Values["number"]}");
+                 });
+
+            });
+        }
+    }
+}
