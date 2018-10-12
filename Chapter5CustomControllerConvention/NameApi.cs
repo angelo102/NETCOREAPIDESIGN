8550f0817a5c91727dc1ce5979923866f2f07cd4
Angel Romero <aromero@isn.com>
2018-10-12 15:29:38 -0500

    Chapter 5 custom contoller convention

---
+++ Chapter5CustomControllerConvention/NameApi.cs
@@ -0,0 +1,24 @@
+﻿using Chapter5CustomControllerConvention;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.People
+{
+    public class NameApi
+    {
+        private readonly IPeopleRepository people;
+
+        public NameApi(IPeopleRepository people)
+        {
+            this.people = people;
+        }
+
+        public IActionResult Get()
+        {
+            return new OkObjectResult(people.All);
+        }
+    }
+}
---
+++ Chapter5CustomControllerConvention/PeopleRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chapter5CustomControllerConvention
+{
+    public class PeopleRepository : IPeopleRepository
+    {
+        public IEnumerable<string> All => new string[] { "Fanie", "John", "Joe" };
+    }
+}
