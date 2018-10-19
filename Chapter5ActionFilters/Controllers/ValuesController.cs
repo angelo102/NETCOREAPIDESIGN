ae0becedc2380cd24b435ad007d85ba5332f5c35
Angel Romero <aromero@isn.com>
2018-10-19 15:00:31 -0500

    Chapter 5 Filter Example

---
+++ Chapter5ActionFilters/Controllers/ValuesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chapter5ActionFilters.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+   
+        public string Get(string timestamp)
+        {
+            return $"Request received at {timestamp}";
+        }
+
+       
+    }
+}
